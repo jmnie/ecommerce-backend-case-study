9394d43274377195ba1a23a79924d8e084c66752
Jiaming Nie <jiaming.nie13@gmail.com>
2023-08-24 21:20:26 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
-const app_module_1 = require("./modules/app/app.module");
+const app_module_1 = require("./app/app.module");
 const app_config_1 = require("./config/app.config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
--- src/config/middleware.config.ts
+++ src/config/middleware.config.ts
@@ -4,7 +4,7 @@ const MiddlewareConfig = {
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'password',
+    password: 'temp_password',
     database: 'mysql',
   },
   redisCache: {
